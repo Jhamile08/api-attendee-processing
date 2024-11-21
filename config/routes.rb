435ec9140a6ee75d4d20fef9b9d6b6326a87257a
jhamile08 <jhamiledr222@gmail.com>
2024-11-21 10:56:40 -0500

    api implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-   
-  #this is for documentation of api with Oasrails
+
+  # this is for documentation of api with Oasrails
   mount OasRails::Engine => '/docs'
+
+  # resources :event do
+  #   resources :attendees, [:summary] # /events/:event_id/attendees, # /events/:event_id/attendees/summary
+  # end
+
+  # resources :attendees do
+  #   resources :confirm # /attendees/:attendee_id/confirm
+  # end
 end
