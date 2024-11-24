1f58425e8999857a8cbb3f4d3587845358cbe23a
jhamile08 <jhamiledr222@gmail.com>
2024-11-24 18:08:03 -0500

    add validations

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,10 +1,11 @@
 class Attendee < ApplicationRecord
+  # Relaciones
   belongs_to :user_attendee
-
   has_many :attendee_logs, dependent: :destroy
 
-  validates :ticket_id, :name, :email, presence: true
-  validates :status, inclusion: { in: [true, false] }
-
-  belongs_to :user_attendee
+  # Validaciones
+  validates :ticket_id, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :status, inclusion: { in: [true, false], message: 'must be true or false' }
 end
--- app/models/user_attendee.rb
+++ app/models/user_attendee.rb
@@ -1,6 +1,10 @@
 class UserAttendee < ApplicationRecord
+  # Relaciones
   has_many :attendees, dependent: :destroy
   has_many :attendee_rules, dependent: :destroy
 
-  validates :quantity_tickets, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  # Validaciones
+  validates :quantity_tickets,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
