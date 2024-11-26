bd5c2811f889d0763f9b9d7a25d12d47d0f07159
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-11-26 12:20:23 -0500

    pagination starting with size 1

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,6 +1,9 @@
 class AttendeesController < ApplicationController
 
-  @summary "Returns a list of Users."
+  # @summary Returns a list of Attendees.
+  # 
+  # @parameter size(query) [Integer]  Used for pagination of response data (default: 5 items per response).
+  # @parameter page(query) [Integer]   choose the numer of pages you want to see
   def summary
     
     #params[:event_id] returns the id of the event sent in the URL
@@ -9,32 +12,108 @@ class AttendeesController < ApplicationController
     @size = params[:size] || 5
     @page = params[:page] || 1
 
-    puts "This is size: #{params[:size]} and this is page #{params[:page]}"
+    puts "THIS IS SIZE: #{params[:size].class} and this is page #{params[:page].class}"
+
+    #when send page and size convert to string
+    
+    if params[:size] != nil
+      @size = params[:size].to_i
+    end
+
+    if params[:page] != nil
+      @page = params[:page].to_i
+    end
+
 
     #with this consult we will find the attendee rules that belongs to that specify event and with the attendee_rules filtered
     #now he have the attendee_rule where we can get  the user_attendee_id for filter by user_attendee_id that belongs to that specific event
-    @attendee_rules = AttendeeRule.where(event_id: params[:event_id])
+    @attendees = Attendee.all
 
     #now we have to collect all the attendees to the event cause we are finding attendies separectly by user_attendee_id
     all_attendees_for_event = []
 
+    #Simulating tickets
+    tickets = {
+      'tickets' => [
+        { id: 1, event_id:  1 },
+        { id: 2, event_id:  1 },
+        { id: 3, event_id:  1 },
+        { id: 4, event_id:  1 },
+        { id: 5, event_id:  1 },
+        { id: 6, event_id:  2 },
+        { id: 7, event_id:  2 },
+        { id: 8, event_id:  2 },
+      ]
+    }
+    
+
     true_attendees = 0;
     false_attendees = 0;
-    @attendee_rules.each do |attendee_rule|
-      @attendees = Attendee.where(user_attendee_id: attendee_rule.user_attendee_id).to_a
-
-      all_attendees_for_event.concat(@attendees)
-
-      #count true attendees and false attendees
-      @attendees.each do |attendee| 
-        if attendee.status then
-          true_attendees += 1
-        else
-          false_attendees += 1
+    
+    @attendees.each do |attendee|
+      
+      puts attendee
+      tickets['tickets'].each do |ticket| 
+        puts "Este es el tiquete de base de datos #{(attendee.ticket_id).to_i.class}"
+        puts "Este es el tiquete de json #{ticket[:id].class}"
+        if (attendee.ticket_id).to_i == ticket[:id]
+          puts "Entro al padre"
+          if params[:event_id].to_i == ticket[:event_id] 
+            puts "EVENTO SIMILARES"
+            all_attendees_for_event << attendee
+            if attendee.status 
+              true_attendees += 1
+            else
+              false_attendees += 1
+            end
+          else
+            puts "NO ENTRO"
+          end
         end
       end
+      
+    end
 
-      puts "This is attendents #{@attendees}"
+    #pagination
+    #
+    attendees_paginate = []
+    attendees_paginate_show = []
+
+
+    page_count = 0
+    puts "HWYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+    all_attendees_for_event.each_with_index do |attendee, index|
+      puts "entro al ciclo"
+      puts "size #{@size*@page}"
+      puts "index #{index}"
+      if index < (@size*@page)
+        puts "SIIIIIIIIIIIIIIIIIII"
+        if true
+          if ((index+1) % @size) == 0
+            attendees_paginate << attendee
+            puts "Seteo"
+            page_count += 1
+            page = "page" + ((index + 1)/@size).to_s
+            puts "THIS IS PAGEEEEEEE #{page}"
+            pageNumber = {
+              page => {
+                attendees: attendees_paginate
+              }
+            }
+            attendees_paginate = []
+
+            puts "pageNumber #{pageNumber}"
+            attendees_paginate_show << pageNumber
+          else
+            attendees_paginate << attendee
+          end
+        else
+          attendees_paginate << attendee
+        end
+      else
+        break;
+      end
+
     end
     
     puts "Number of attendees that will come: #{true_attendees}"
@@ -53,6 +132,6 @@ class AttendeesController < ApplicationController
                                  tickets_not_sold: (event_capacity-total_registered)
                                 }
     
-    render json: all_attendees_for_event
+    render json: attendees_paginate_show
   end
 end
