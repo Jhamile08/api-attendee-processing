ea1b3fd2bb8d7f4ddc71bdaafc731a0fd9b1bfd9
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-11-26 09:57:28 -0500

    getting parameters from params in the url

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,8 +1,15 @@
 class AttendeesController < ApplicationController
 
+  @summary "Returns a list of Users."
   def summary
     
     #params[:event_id] returns the id of the event sent in the URL
+    #
+    # Params for pagination
+    @size = params[:size] || 5
+    @page = params[:page] || 1
+
+    puts "This is size: #{params[:size]} and this is page #{params[:page]}"
 
     #with this consult we will find the attendee rules that belongs to that specify event and with the attendee_rules filtered
     #now he have the attendee_rule where we can get  the user_attendee_id for filter by user_attendee_id that belongs to that specific event
