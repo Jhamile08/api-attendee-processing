e9e9dc1ca3218ea254bc888e46ce6e12e44f8b56
jhamile08 <jhamiledr222@gmail.com>
2024-11-28 15:06:28 -0500

    pull requests completed task 004

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   # POST /events/:event_id/attendees
   get '/events/:event_id/attendees' => 'attendees#create_attendees'
+  get 'user_attendees/show_user_attendees', to: 'user_attendees#show_user_attendees'
+  get '/events/:event_id/attendees/summary' => 'attendees#summary'
+  get '/events/:event_id/attendees/summary/assistants' => 'attendees#assistants'
 
   #  PATCH /attendees/:attendee_id/confirm, /attendees/:attendee_id/cancel, GET /attendees/:attendee_id/logs`
 
@@ -24,43 +27,4 @@ Rails.application.routes.draw do
       get :logs
     end
   end
-
-  get 'user_attendees/show_user_attendees', to: 'user_attendees#show_user_attendees'
-
-  # resources :attendees do
-  #   patch :"confirm", :"cancel"
-  #   get :"logs"
-  # end
-
-  # New Routes
-  resources :attendees do
-    member do # Member block the routes for individual attendees (using :id)
-      patch :confirm
-      patch :cancel
-      get :log
-    end
-  end
-
-  # resources :user_attendees do
-  #   member do
-  #     patch :cancel_attendees
-  #     patch :cancel_attendee
-  #   end
-  # end
-
-  #  GET /events/:event_id/attendees/summary
-
-  get '/events/:event_id/attendees/summary' => 'attendees#summary'
-  #  get '/events/:event_id/attendees/summary' => 'attendees#summary', as: :rails_health_check
-  get '/events/:event_id/attendees/summary/assistants' => 'attendees#assistants'
-
-  #
-
-  # GET /attendees/:attendee_id/logs
-  # get 'attendees/:attendee_id/logs', to: 'attendees#logs', as: 'attendee_logs'
-  # resources :attendees do
-  #   member do
-  #     get :"logs"
-  #   end
-  # end
 end
